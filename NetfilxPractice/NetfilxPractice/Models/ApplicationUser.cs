74d1ddde6b733e994bb98c90040beb5f3a3bc4ea
musab <yaqoobmusab03@gmail.com>
2022-03-17 12:24:15 -0500

    initial starter files and ApplicationUser Class

---
+++ NetfilxPractice/NetfilxPractice/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NetfilxPractice.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //Add new properties to IdentityUser
+    }
+}
